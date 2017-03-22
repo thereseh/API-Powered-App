c7b32c20aac128b0671ed53625b95adfc0043c59
Therese Henriksson <tah5279@rit.edu>
2017-03-22 13:55:32 -0400

    change

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -3,6 +3,7 @@ const crypto = require('crypto');
 
 // Note this object is purely in memory
 const recipe = {};
+const categories = { Miscellaneous: 'Miscellaneous' };
 
 // sha1 is a bit of a quicker hash algorithm for insecure things
 let etag = crypto.createHash('sha1').update(JSON.stringify(recipe));
@@ -30,11 +31,29 @@ const respondJSONMeta = (request, response, status) => {
   response.end();
 };
 
+// removes a specific recipe
+const removeRecipe = (request, response, body) => {
+  if (recipe[body.id]) {
+    delete recipe[body.id];
+  }
+  // update etag since a change has been made
+  etag = crypto.createHash('sha1').update(JSON.stringify(recipe));
+  digest = etag.digest('hex');
+  const responseJSON = {
+    recipe,
+  };
+  if (request.headers['if-none-match'] === digest) {
+    console.log(request.headers['if-none-match']);
+
+    return respondJSONMeta(request, response, 304);
+  }
+  return respondJSON(request, response, 200, responseJSON);
+};
+// returns all recipes
 const getRecipe = (request, response) => {
   const responseJSON = {
     recipe,
   };
-
   if (request.headers['if-none-match'] === digest) {
     console.log(request.headers['if-none-match']);
 
@@ -43,46 +62,39 @@ const getRecipe = (request, response) => {
 
   return respondJSON(request, response, 200, responseJSON);
 };
-
+// adds a new recipe
 const addRecipe = (request, response, body) => {
   // default json message
   const responseJSON = {
     message: 'Name and age are both required.',
   };
-  console.log('adding recipe');
-
-  // check to make sure we have both fields
-  // We might want more validation than just checking if they exist
-  // This could easily be abused with invalid types (such as booleans, numbers, etc)
-  // If either are missing, send back an error message as a 400 badRequest
-  if (!body.name) {
-    responseJSON.id = 'missingParams';
-    return respondJSON(request, response, 400, responseJSON);
-  }
-
   // default status code to 201 created
   let responseCode = 201;
 
   // if that user's name already exists in our object
   // then switch to a 204 updated status
-  if (recipe[body.name]) {
-    recipe[body.name].ingredients = body.ingredients;
-    recipe[body.name].category = body.category;
+  if (recipe[body.id]) {
+    recipe[body.id].ingredients = body.ingredients;
+    recipe[body.id].category = body.category;
+    recipe[body.id].notes = body.notes;
+    recipe[body.id].name = body.name;
+    recipe[body.id].color = body.color;
     responseCode = 204;
   } else {
     // otherwise create an object with that name
-    recipe[body.name] = {};
+    recipe[body.id] = {};
   }
-
-  // add or update fields for this user name
-  recipe[body.name].name = body.name;
-  recipe[body.name].ingredients = body.ingredients;
-  recipe[body.name].category = body.category;
-  console.log(`recipe ${recipe}`);
-
-  console.log(`body.name: ${body.name}`);
-  console.log(`body.ing: ${body.ingredients}`);
-  console.log(`body.cat: ${body.category}`);
+  // check if a new category has been added
+  if (!categories[body.category]) {
+    categories[body.category] = body.category;
+  }
+  // add or update fields for this recipe
+  recipe[body.id].id = body.id;
+  recipe[body.id].name = body.name;
+  recipe[body.id].ingredients = body.ingredients;
+  recipe[body.id].category = body.category;
+  recipe[body.id].notes = body.notes;
+  recipe[body.id].color = body.color;
 
   etag = crypto.createHash('sha1').update(JSON.stringify(recipe));
   digest = etag.digest('hex');
@@ -90,13 +102,35 @@ const addRecipe = (request, response, body) => {
   // if response is created, then set our created message
   // and sent response with a message
   if (responseCode === 201) {
+    responseJSON.category = categories;
     responseJSON.message = 'Created Successfully';
     return respondJSON(request, response, responseCode, responseJSON);
   }
   // 204 has an empty payload, just a success
-  // It cannot have a body, so we just send a 204 without a message
   // 204 will not alter the browser in any way!!!
   return respondJSONMeta(request, response, responseCode);
+};
+// returns all categories for dropdown
+const getCategory = (request, response, body) => {
+  const temp = [];
+  Object.keys(recipe).forEach = (key) => {
+    const value = recipe[key].category;
+    if (value === body.category) {
+      temp.push(recipe[key]);
+    }
+  };
+  recipe.find(body.category);
+  const responseJSON = {
+    temp,
+  };
+  console.dir(temp);
+  if (request.headers['if-none-match'] === digest) {
+    console.log(request.headers['if-none-match']);
+
+    return respondJSONMeta(request, response, 304);
+  }
+
+  return respondJSON(request, response, 200, responseJSON);
 };
 
 const notFound = (request, response) => {
@@ -117,6 +151,8 @@ const notFoundMeta = (request, response) =>
 module.exports = {
   getRecipe,
   addRecipe,
+  removeRecipe,
   notFound,
   notFoundMeta,
+  getCategory,
 };
--- src/server.js
+++ src/server.js
@@ -7,45 +7,51 @@ const query = require('querystring');
 const PORT = process.env.PORT || process.env.NODE_PORT || 3000;
 
 // handle POST requests
-const handlePost = (request, response, parsedUrl) => {
+const handleParams = (request, response, parsedUrl) => {
   // if post is to /addUser (our only POST url)
-  if (parsedUrl.pathname === '/addRecipe') {
-    const res = response;
-    // uploads come in as a byte stream that we need
-    // to reassemble once it's all arrived
-    const body = [];
-    // if the upload stream errors out, just throw a
-    // a bad request and send it back
-    request.on('error', (err) => {
-      console.dir(err);
-      res.statusCode = 400;
-      res.end();
-    });
-    // on 'data' is for each byte of data that comes in
-    // from the upload. We will add it to our byte array.
-    request.on('data', (chunk) => {
-      body.push(chunk);
-    });
-    // on end of upload stream.
-    request.on('end', () => {
-      // combine our byte array (using Buffer.concat)
-      // and convert it to a string value (in this instance)
-      const bodyString = Buffer.concat(body).toString();
-      // since we are getting x-www-form-urlencoded data
-      // the format will be the same as querystrings
-      // Parse the string into an object by field name
-      const bodyParams = query.parse(bodyString);
-      // pass to our addUser function
+  const res = response;
+  // uploads come in as a byte stream that we need
+  // to reassemble once it's all arrived
+  const body = [];
+  // if the upload stream errors out, just throw a
+  // a bad request and send it back
+  request.on('error', (err) => {
+    console.dir(err);
+    res.statusCode = 400;
+    res.end();
+  });
+  // on 'data' is for each byte of data that comes in
+  // from the upload. We will add it to our byte array.
+  request.on('data', (chunk) => {
+    body.push(chunk);
+  });
+  // on end of upload stream.
+  request.on('end', () => {
+    // combine our byte array (using Buffer.concat)
+    // and convert it to a string value (in this instance)
+    const bodyString = Buffer.concat(body).toString();
+    // since we are getting x-www-form-urlencoded data
+    // the format will be the same as querystrings
+    // Parse the string into an object by field name
+    const bodyParams = query.parse(bodyString);
+    // pass to our addUser function
+    if (parsedUrl.pathname === '/addRecipe') {
       jsonHandler.addRecipe(request, res, bodyParams);
-    });
-  }
+    }
+    if (parsedUrl.pathname === '/removeRecipe') {
+      jsonHandler.removeRecipe(request, res, bodyParams);
+    }
+    if (parsedUrl.pathname === '/getCategory') {
+      jsonHandler.getCategory(request, res, bodyParams);
+    }
+  });
 };
 
 const onRequest = (request, response) => {
   const parsedUrl = url.parse(request.url);
   switch (request.method) {
     case 'POST':
-      handlePost(request, response, parsedUrl);
+      handleParams(request, response, parsedUrl);
       break;
     case 'GET':
       if (parsedUrl.pathname === '/') {
@@ -54,12 +60,17 @@ const onRequest = (request, response) => {
         htmlHandler.getCSS(request, response);
       } else if (parsedUrl.pathname === '/getRecipe') {
         jsonHandler.getRecipe(request, response);
+      } else if (parsedUrl.pathname === '/getCategory') {
+        handleParams(request, response, parsedUrl);
       } else {
         jsonHandler.notFound(request, response);
       }
       break;
     case 'HEAD':
       jsonHandler.notFoundMeta(request, response);
+      break;
+    case 'DELETE':
+      handleParams(request, response, parsedUrl);
       break;
     default:
       jsonHandler.notFoundMeta(request, response);
